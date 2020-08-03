4fa4f9ad1fc2187c4193ddb830f81ce4fb9558eb
bkkeerthana96 <68589444+bkkeerthana96@users.noreply.github.com>
2020-08-03 18:07:17 +0530

    Update TestVehicleParams.cpp

--- TestVehicleParams.cpp
+++ TestVehicleParams.cpp
@@ -5,7 +5,7 @@
 TEST(Translate_Veh_Params, when_a_parameter_received_from_a_vehicle_should_update_aginst_vechile_id) 
 { 
  telematics Veh_received_param = {231, motor_temp, 30};
- inventory Veh_params = Translate_Veh_info(Veh_received_param);
+ inventory Veh_params = Translate_Veh_info(&Veh_received_param);
  ASSERT_EQ(Veh_params.motor_temp, 30); 
 }
 
