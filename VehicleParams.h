e834278e6ae44973cc0a6ff50c3fca30fc03ebf4
bkkeerthana96 <68589444+bkkeerthana96@users.noreply.github.com>
2020-08-03 17:01:40 +0530

    Create VehicleParams.h

---
+++ VehicleParams.h
@@ -0,0 +1,23 @@
+enum measure
+{
+  motor_temp,
+  battery_pc,
+  battery_temp
+};
+
+struct telematics
+{
+  int vehicle_id;
+  measure type;
+  float measurement;
+};
+
+struct inventory
+{
+  int vehicle_id;
+  float motor_temp;
+  float battery_pc;
+  float battery_temp;
+ };
+ 
+ inventory Translate_Veh_info(telematics Veh_received_param);
