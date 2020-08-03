42e78da77c352d1bd3ba4bb70ccb22425ac8c5e5
bkkeerthana96 <68589444+bkkeerthana96@users.noreply.github.com>
2020-08-03 18:11:19 +0530

    Update VehicleParams.cpp

--- VehicleParams.cpp
+++ VehicleParams.cpp
@@ -1,10 +1,9 @@
 #include "VehicleParams.h"
-#include <limits>
-
-std::numeric_limits<double>::quiet_NaN()
  
- inventory Translate_Veh_info(telematics* Veh_received_param){
+ inventory Translate_Veh_info(telematics* Veh_received_param)
+{
       inventory store_params;
+ 
       store_params.vehicle_id = Veh_received_param->vehicle_id;
       
       switch(Veh_received_param.type)
