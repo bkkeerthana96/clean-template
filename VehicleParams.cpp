412b9fe210e0b4f21ac8af813a56f5930cec750c
bkkeerthana96 <68589444+bkkeerthana96@users.noreply.github.com>
2020-08-03 18:05:23 +0530

    Update VehicleParams.cpp

--- VehicleParams.cpp
+++ VehicleParams.cpp
@@ -3,20 +3,20 @@
 
 std::numeric_limits<double>::quiet_NaN()
  
- inventory Translate_Veh_info(telematics Veh_received_param){
+ inventory Translate_Veh_info(telematics* Veh_received_param){
       inventory store_params;
-      store_params.vehicle_id = Veh_received_param.vehicle_id;
+      store_params.vehicle_id = Veh_received_param->vehicle_id;
       
       switch(Veh_received_param.type)
       {
         case motor_temp:
-          store_params.motor_temp = Veh_received_param.measurement;
+          store_params.motor_temp = Veh_received_param->measurement;
           break;
         case battery_pc:
-          store_params.motor_temp = Veh_received_param.measurement;
+          store_params.motor_temp = Veh_received_param->measurement;
           break;
         case battery_temp:
-          store_params.motor_temp = Veh_received_param.measurement;
+          store_params.motor_temp = Veh_received_param->measurement;
           break;
         case 
       }
