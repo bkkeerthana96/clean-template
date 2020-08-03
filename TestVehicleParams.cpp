f92bcffb80bb6464a773cbe19e6ae8cfe8f8d828
bkkeerthana96 <68589444+bkkeerthana96@users.noreply.github.com>
2020-08-03 16:42:15 +0530

    Create TestVehicleParams.cpp

---
+++ TestVehicleParams.cpp
@@ -0,0 +1,15 @@
+#include <gtest/gtest.h>
+#include "VehicleParams.h"
+
+
+TEST(VitalsTest, when_a_parameter_received_from_a_vehicle_should_update_aginst_vechile_id) 
+{ 
+ telemantics Veh_received_param = {231, motor_temp, 30};
+ inventory Veh_params = Update_Veh_info(&Veh_received_param);
+ ASSERT_EQ( Veh_params.motor_temp, 30); 
+}
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
