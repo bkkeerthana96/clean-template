abd80ed9a693edec64184522ef68319cc9bc69d5
bkkeerthana96 <68589444+bkkeerthana96@users.noreply.github.com>
2020-08-03 17:22:56 +0530

    Create VehicleParams.cpp

---
+++ VehicleParams.cpp
@@ -0,0 +1,22 @@
+#include "VehicleParams.h"
+
+ inventory Translate_Veh_info(telematics Veh_received_param){
+      inventory store_params = {231, 30, NaN, NaN};
+      store_params.vehicle_id = Veh_received_param.vehicle_id;
+      
+      switch(Veh_received_param.type)
+      {
+        case motor_temp:
+          store_params.motor_temp = Veh_received_param.measurement;
+          break;
+        case battery_pc:
+          store_params.motor_temp = Veh_received_param.measurement;
+          break;
+        case battery_temp:
+          store_params.motor_temp = Veh_received_param.measurement;
+          break;
+        case 
+      }
+      
+      return store_params;
+ }
