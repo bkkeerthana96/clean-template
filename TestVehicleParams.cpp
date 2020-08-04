c7326baf1bd71b75f0138e26689d3e589688bd0a
bkkeerthana96 <68589444+bkkeerthana96@users.noreply.github.com>
2020-08-04 20:05:51 +0530

    Update TestVehicleParams.cpp

--- TestVehicleParams.cpp
+++ TestVehicleParams.cpp
@@ -12,7 +12,13 @@ TEST(Translate_Veh_Params, when_a_battery_received_from_a_vehicle_should_update_
 { 
  telematics Veh_received_param = {231, battery_temp, 70};
  inventory Veh_params = Translate_Veh_info(&Veh_received_param);
- ASSERT_EQ(Veh_params.motor_temp, 70); 
+ ASSERT_EQ(Veh_params.battery_temp, 70); 
+}
+TEST(Translate_Veh_Params, when_a_battery_pc_received_from_a_vehicle_should_update_aginst_vechile_id) 
+{ 
+ telematics Veh_received_param = {231, battery_pc, 50};
+ inventory Veh_params = Translate_Veh_info(&Veh_received_param);
+ ASSERT_EQ(Veh_params.battery_pc, 50); 
 }
 
 int main(int argc, char **argv) {
