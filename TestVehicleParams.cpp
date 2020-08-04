5ce66e3b55c5c18123b795634779d3a076c9894e
bkkeerthana96 <68589444+bkkeerthana96@users.noreply.github.com>
2020-08-04 16:43:58 +0530

    Update TestVehicleParams.cpp

--- TestVehicleParams.cpp
+++ TestVehicleParams.cpp
@@ -2,11 +2,17 @@
 #include "VehicleParams.h"
 
 
-TEST(Translate_Veh_Params, when_a_parameter_received_from_a_vehicle_should_update_aginst_vechile_id) 
+TEST(Translate_Veh_Params, when_a_temp_received_from_a_vehicle_should_update_aginst_vechile_id) 
 { 
  telematics Veh_received_param = {231, motor_temp, 30};
  inventory Veh_params = Translate_Veh_info(&Veh_received_param);
  ASSERT_EQ(Veh_params.motor_temp, 30); 
+}
+TEST(Translate_Veh_Params, when_a_battery_received_from_a_vehicle_should_update_aginst_vechile_id) 
+{ 
+ telematics Veh_received_param = {231, battery_temp, 70};
+ inventory Veh_params = Translate_Veh_info(&Veh_received_param);
+ ASSERT_EQ(Veh_params.motor_temp, 70); 
 }
 
 int main(int argc, char **argv) {
